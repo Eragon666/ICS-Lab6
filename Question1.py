c6f0fb7d27f853dda86f554f2433866c677ba1e9
Matthijs <matthijsthoolen@hotmail.com>
2015-10-17 16:06:48 +0200

    Question 1b done

--- Question1.py
+++ Question1.py
@@ -25,11 +25,26 @@ class erdos:
         # Create a graph with N = self.N and <k> = self.k
         self.G = nx.fast_gnp_random_graph(self.N, self.k / self.N)
 
-        # Infect 0.01% of the vertices, random.
-        self.infected = (np.random.rand(self.N) < 0.01).astype(int)
+        # Infect 0.01% of the vertices, random
+        nrinfected = int(float(self.N)*float(0.001))
+        infected = random.sample(xrange(0, self.N), nrinfected)
 
-    def step(self):
+        self.infected = np.zeros((self.N,), dtype=np.int)
+
+        for val in infected:
+            self.infected[infected] = 1
+
+        # This function resulted in not exactly 0.01% of infected vertexes
+        # self.infected = (np.random.rand(self.N) <= 0.001).astype(int)
+
+    def step(self, t):
         """ Do the calculation for this time step """
+
+        self.t = t
+
+        # Make a copy of self.infected to make sure that only vertexes that
+        # were already infected can infect other vertexes in this step.
+        infected = np.copy(self.infected)
 
         for j in xrange(0, self.N):
 
@@ -42,12 +57,13 @@ class erdos:
             # Loop through all the neighbors, and count infected
             for neighbor in neighbors:
 
-                if (self.infected[neighbor] == 0):
+                if (infected[neighbor] == 0):
                     self.checkInfected(neighbor)
 
-        self.prevalence[self.t][self.repeat] = float(sum(self.infected))/float(self.N)
+    def calcPrevalence(self):
+        """ Calculate the prevalence """
 
-        self.t += 1
+        self.prevalence[self.t][self.repeat] = sum(self.infected)/float(self.N)
 
     def checkInfected(self, vertex):
         """ Check if the given vertex is infected in the next timestep """
@@ -63,8 +79,9 @@ class erdos:
 def printGraph(prevalence, tTotal, repeat, label, stepsize = 10):
     """ Add the errorbarplot for this simulation to the figure """
 
-    y = np.empty([int(tTotal/stepsize)], dtype=float)
     x = np.arange(0, tTotal, stepsize)
+    y = np.empty([int(tTotal/stepsize)], dtype=float)
+    std = np.empty([int(tTotal/stepsize)], dtype=float)
 
     j = 0
 
@@ -74,13 +91,14 @@ def printGraph(prevalence, tTotal, repeat, label, stepsize = 10):
         if (i % stepsize == 0):
             step = prevalence[i]
             y[j] = np.average(step)
+            std[j] = np.std(step)
             j += 1
 
     # plot the errorbar
-    plt.errorbar(x, y, label=label)
+    plt.errorbar(x, y, yerr=std, label=label)
 
 
-def runSim(tTotal, repeat, N, k, i):
+def runSim(tTotal, repeat, N, i, k):
     """ Run the simulation with the given settings"""
 
     # Make an array to save the data
@@ -88,10 +106,16 @@ def runSim(tTotal, repeat, N, k, i):
 
     for j in xrange(0, repeat):
 
-        graph = erdos(10**4, 0.01, 5.0, prevalence, j)
+        print str(j+1) + '/' + str(repeat)
 
-        for i in xrange(0, tTotal):
-            graph.step()
+        graph = erdos(N, i, k, prevalence, j)
+
+        # Save the prevalence of step 0
+        graph.calcPrevalence()
+
+        for l in xrange(1, tTotal):
+            graph.step(l)
+            graph.calcPrevalence()
 
         prevalence = graph.getStats()
 
@@ -99,21 +123,28 @@ def runSim(tTotal, repeat, N, k, i):
 
 def main():
 
-    tTotal = 100
-    repeat = 10
+    tTotal = 40
+    repeat = 50
 
     # Run the two given simulations
+    print 'Running simulation 1/2'
     sim1 = runSim(tTotal, repeat, 10**5, 0.01, 5.0)
+    print 'Running simulation 2/2'
     sim2 = runSim(tTotal, repeat, 10**5, 0.1, 0.8)
 
     plt.figure()
 
-    printGraph(sim1, tTotal, repeat, "N=10^5, i = 0.01, <k> = 5.0", stepsize=10)
-    printGraph(sim2, tTotal, repeat, "N=10^5, i = 0.1, <k> = 0.8", stepsize=10)
+    printGraph(sim1, tTotal, repeat, "N=10^5, i = 0.01, <k> = 5.0", stepsize=1)
+    printGraph(sim2, tTotal, repeat, "N=10^5, i = 0.1, <k> = 0.8", stepsize=1)
 
     plt.legend()
 
     plt.title("Question 1b with 2 simulations")
+
+    print """ The figure shows the two simulations needed for question 2. Every
+    simulation is simulated with """ + str(tTotal) + """ steps and this is repeated
+    """ + str(repeat) + """ times. """
+
     plt.show()
 
 
